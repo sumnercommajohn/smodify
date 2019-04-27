749795aca4671c87595ba84bf1eacef4c1697ed7
John Sumner <sumnercommajohn@gmail.com>
2019-04-27 02:51:10 -0400

    add eslint to project - using airbnb style guide

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Welcome extends React.Component {
-    render() {
-        return <h1>Welcome To Shmotify</h1>;
-    }
+  render() {
+    return <h1>Welcome To Shmotify</h1>;
+  }
 }
+
 ReactDOM.render(<Welcome />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: `${__dirname}/dist`,
-        publicPath: '/',
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: './dist',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve('./index.html'),
-        }),
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html'),
+    }),
+  ],
 };
