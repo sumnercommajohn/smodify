615d4025dae39d6fe655f50e50e547178ca726fd
John Sumner <sumnercommajohn@gmail.com>
2019-04-27 23:53:35 -0400

    updated App image

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import macaroon from '../assets/img/Macaroonicon.png';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <h1 className="title">Shmotify</h1>
-        <img src={macaroon} alt="" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">Shmotify.</h1>
+      <img src={macaroon} alt="" />
+    </div>
+  );
 }
 
 export default hot(App);
