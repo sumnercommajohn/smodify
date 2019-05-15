ed4fb4930b6daf26a26ee15a2ba97ab45082ed89
John Sumner <sumnercommajohn@gmail.com>
2019-05-15 18:42:04 -0400

    set up prototype authorization flow in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,70 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import macaroon from '../assets/img/Macaroonicon.png';
+import authQuery from '../config/authConfig';
+import getHash from '../config/hash';
 
-function App() {
-  return (
-    <div className="app">
-      <h1 className="title">Shmotify.</h1>
-      <img src={macaroon} alt="" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    token: null,
+    user: null,
+  }
+
+  componentDidMount() {
+    console.log(this.state.token);
+    if (!this.state.token) {
+      this.setToken();
+    }
+  }
+
+  setToken = () => {
+    const token = getHash();
+    this.setState({ token });
+  }
+
+  getProfile = () => {
+    const authToken = this.state.token;
+    const myHeaders = new Headers();
+
+    myHeaders.append('Authorization', `Bearer ${authToken}`);
+
+    fetch('https://api.spotify.com/v1/me', {
+      method: 'GET',
+      headers: myHeaders,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw Error(`Request rejected with status ${response.status}`);
+      })
+      .then((user) => {
+        this.setState({ user });
+      });
+  }
+
+  renderLoginLink = () => (
+    <a href={authQuery}>Login to Spotify</a>
+  )
+
+  renderWelcome = () => {
+    const displayName = this.state.user.display_name;
+    return (
+      <h3>Welcome, {displayName}</h3>
+    );
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <img src={macaroon} alt="" />
+        <h1 className="title">Smodify.</h1>
+        { this.state.user ? this.renderWelcome() : this.renderLoginLink() }
+        <button type="button" onClick={this.getProfile}>Get Profile Data</button>
+      </div>
+    );
+  }
 }
 
 export default hot(App);
