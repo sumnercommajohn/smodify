7ce91e039f12faec587a95309cc53949683f5cf0
John Sumner <sumnercommajohn@gmail.com>
2019-05-15 18:41:33 -0400

    Add config files for Spotify implicit grant auth flow

---
+++ src/config/authConfig.js
@@ -0,0 +1,14 @@
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = '10a41ddfc787418f9ef272f0bf886e86';
+const redirectUri = 'http://localhost:8080/';
+const scopes = [
+  'user-read-email',
+  'user-read-private',
+  'user-read-birthdate',
+];
+const responseType = 'token';
+const authQuery = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=${responseType}&show_dialog=true`;
+
+export default authQuery;
