0ad3341d2d4c3633dc67a98da8f53ee7c90b00fa
John Sumner <sumnercommajohn@gmail.com>
2019-06-10 18:29:16 -0400

    make Sidebar component, render user playlists inside it

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 export const PlaylistItem = (props) => {
   console.log(props.playlist);
-  const { name, tracks, owner } = props.playlist;
-  console.log(name, tracks, owner);
+  const { name, tracks } = props.playlist;
+  console.log(name, tracks);
   return (
     <li className="playlist-item">
       <span className="playlist-title">{name}</span>
-      <span className="owner">Created by {owner.display_name}</span>
       <span className="tracks">{tracks.total} Songs</span>
     </li>
   );
---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export const Sidebar = props => (
+  <nav className="sidebar">
+    {props.children}
+  </nav>
+);
