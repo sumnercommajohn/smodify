54f3deb56117cd80b1f5d3f772df628c803a1ded
John Sumner <sumnercommajohn@gmail.com>
2019-06-11 18:03:41 -0400

    refactor state in App

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 export const PlaylistItem = (props) => {
-  console.log(props.playlist);
   const { name, tracks } = props.playlist;
-  console.log(name, tracks);
   return (
     <li className="playlist-item">
       <span className="playlist-title">{name}</span>
