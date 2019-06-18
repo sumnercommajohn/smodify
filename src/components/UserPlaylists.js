8192b36c60ef9a65f43c638765749f0714d3eb88
John Sumner <sumnercommajohn@gmail.com>
2019-06-18 19:22:11 -0400

    make UserPlaylist in charge of fetching playlists

--- src/components/UserPlaylists.js
+++ src/components/UserPlaylists.js
@@ -4,29 +4,55 @@ import { ErrorMessage } from './ErrorMessage';
 import SortButton from './SortButton';
 
 
-export const UserPlaylists = (props) => {
-  const {
-    playlists, errorMessage, sortPlaylists, setCurrentPlaylist,
-  } = props;
-  return (
-    <div className="sidebar-component">
-      {errorMessage && <ErrorMessage message={errorMessage} />}
-      <h3>Playlists</h3>
-      <ul className="user-playlists">
-        <div className="list-headers">
-          <SortButton sortFunction={sortPlaylists} sortBy="name">Title</SortButton>
-          <SortButton sortFunction={sortPlaylists} sortBy="total">Tracks</SortButton>
-        </div>
-        {playlists.length
-          ? playlists.map(playlist => (
-            <PlaylistItem
-              key={playlist.id}
-              playlist={playlist}
-              setCurrentPlaylist={setCurrentPlaylist}
-            />
-          ))
-          : <p>Loading...</p>}
-      </ul>
-    </div>
-  );
-};
+class UserPlaylists extends React.Component {
+  componentDidMount() {
+    const { token, fetchUserPlaylists } = this.props;
+    console.log('fetching Playlists...');
+    fetchUserPlaylists(token);
+  }
+
+  componentDidUpdate() {
+    const { fetchUserPlaylists, token, playlists: { nextPlaylistsEndpoint } } = this.props;
+    if (nextPlaylistsEndpoint) {
+      console.log('fetching more playlists...');
+      fetchUserPlaylists(token, nextPlaylistsEndpoint);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('UserPlaylists unmounting!');
+  }
+
+  render() {
+    console.log('UserPlaylists render');
+    const {
+      playlists,
+      errorMessage,
+      sortPlaylists,
+      setCurrentPlaylist,
+    } = this.props;
+    return (
+      <div className="sidebar-component">
+        {errorMessage && <ErrorMessage message={errorMessage} />}
+        <h3>Playlists</h3>
+        <ul className="user-playlists">
+          <div className="list-headers">
+            <SortButton sortFunction={sortPlaylists} sortBy="name">Title</SortButton>
+            <SortButton sortFunction={sortPlaylists} sortBy="total">Tracks</SortButton>
+          </div>
+          {playlists.items.length
+            ? playlists.items.map(playlist => (
+              <PlaylistItem
+                key={playlist.id}
+                playlist={playlist}
+                setCurrentPlaylist={setCurrentPlaylist}
+              />
+            ))
+            : <p>Loading...</p>}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UserPlaylists;
